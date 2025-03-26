d2c0cd678d4fab7c196cb0a4f04389feba43ad30
AIMINTU <minto.aminul4@hotmail.com>
2025-03-26 06:47:20 +0100

    project setup and header done

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
+import HeaderImage from '@/assets/dailystar.jpg'
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
+        <div className='text-center p-2 md:p-0 my-7'>
+            <Image className='block mx-auto w-4/6 md:w-3/6' src={HeaderImage} alt='news portal' width={500} height={200}/>
+            <p>Journalist without Fear or Favour</p>
+            <p>Thursday, March 20, 2025</p>
         </div>
     );
 };
